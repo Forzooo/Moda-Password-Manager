25de158a5fceca53e17b1b8376351802dfb6e405
Forzo <96546068+Forzooo@users.noreply.github.com>
2025-07-12 18:14:50 +0200

    Completato Issue #1
    - Creata una classe base per far comunicare i due thread
    - La classe CommunicationHandler non è più al momento un thread, rimane solo un attributo delle classi Backend e Frontend
    - Creata la classe Event

---
+++ src/main/java/moda/passwordManager/communicationHandler/CommunicationHandler.java
@@ -0,0 +1,39 @@
+package moda.passwordManager.communicationHandler;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+// TODO: creare un Issue per l'encryption
+public class CommunicationHandler {
+
+    private LinkedBlockingQueue<Event> sender;  // Send Event from this queue to the other thread
+    private LinkedBlockingQueue<Event> receiver;  // Receive Event from this queue from the other thread
+
+    public CommunicationHandler(LinkedBlockingQueue<Event> sender, LinkedBlockingQueue<Event> receiver){
+        super();
+
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    /**
+     * Send an Event to the other thread. <br/>
+     */
+    public void send(Event event) {
+        try {
+            this.sender.put(event);  // Put the event in the queue
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Receive an Event from the other thread.
+     */
+    public Event receive() {
+        try {
+            return this.receiver.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/moda/passwordManager/communicationHandler/Event.java
@@ -0,0 +1,14 @@
+package moda.passwordManager.communicationHandler;
+
+import java.util.ArrayList;
+
+public class Event {
+
+    private String name;  // The name of the event which specifies which operation to perform
+    private ArrayList data;  // The data that is communicated with the other thread
+
+    public Event(String name, ArrayList data){
+        this.name = name;
+        this.data = new ArrayList(data);
+    }
+}
