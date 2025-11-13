7796997f24ee189d9419b6a79d48fc58ef9c7902
Forzo <96546068+Forzooo@users.noreply.github.com>
2025-11-13 20:54:34 +0100

    Migliorie User Data
    - Spostata la creazione dei Panel per i dati di UserData in una classe separata: UserDataField

--- src/main/java/moda/passwordmanager/frontend/panels/UserData.java
+++ src/main/java/moda/passwordmanager/frontend/panels/UserData.java
@@ -7,12 +7,8 @@ import moda.passwordmanager.interthreadcommunication.Event;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A JPanel that shows the all the information related to an ID of a record of the database
@@ -22,10 +18,9 @@ public class UserData extends JPanel {
     private InterThreadCommunication itc;
 
     private final int ID;  // The ID associated with the data
-    private List<JTextField> dataFields;  // The data field contain the user data
-    private List<JButton> copyButtons;  // The copy buttons for each data field
 
     // Swing Components
+    private UserDataField[] userDataFields;  // The fields of the data
     private JButton modifyButton;
     private JButton saveChangesButton;
     private JButton deleteButton;
@@ -43,7 +38,7 @@ public class UserData extends JPanel {
     }
 
     /**
-     * Set the configuration of the Dialog
+     * Set the configuration of the Panel
      */
     private void initPanel(int width, int height){
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));  // Set its layout
@@ -51,7 +46,6 @@ public class UserData extends JPanel {
 
         // Set the preferred size
         setPreferredSize(new Dimension(width, height));
-
     }
 
     /**
@@ -76,6 +70,9 @@ public class UserData extends JPanel {
         return userData;
     }
 
+    /**
+     * Get the ID associated with this UserData tab
+     */
     public int getID() {
         return ID;
     }
@@ -92,16 +89,17 @@ public class UserData extends JPanel {
         this.generatePasswordButton = new JButton("Generate a Password");
         this.generatePasswordButton.setVisible(false);
 
-        // Add dynamically all the data fields
-        this.dataFields = new ArrayList<>();  // All the data fields are stored here
-        this.copyButtons = new ArrayList<>();  // All the copy buttons are stored here
+        // Create the panel for each field of the data
+        String[] userData = getData().getFullUserData();  // Retrieve the data of the user to know its length
+        this.userDataFields = new UserDataField[userData.length];  // Set the size based on the data
 
-        // Create the data row for each field
-        for (String dataField : getData().getFullUserData()){
-            add(createDataFieldPanel(dataField));
+        for (int i = 0; i < this.userDataFields.length; i++){
+            UserDataField userDataField = new UserDataField(userData[i]);
+            this.userDataFields[i] = userDataField;  // Set the panel to the array
+            add(userDataField);  // Add the panel to the GUI
         }
 
-        // A panel for the buttons
+        // Create a panel for the buttons
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.add(this.modifyButton);
         buttonsPanel.add(this.saveChangesButton);
@@ -109,32 +107,6 @@ public class UserData extends JPanel {
         buttonsPanel.add(this.generatePasswordButton);
 
         add(buttonsPanel);
-    }
-
-    /**
-     * Create the JPanel for each field inside the data
-     * @param dataField The data field which the panel will be created for
-     */
-    private JPanel createDataFieldPanel(String dataField){
-        JPanel dataFieldPanel = new JPanel(new BorderLayout(5, 0));
-        dataFieldPanel.setBorder(new EmptyBorder(5, 0, 5, 0));  // Padding between rows
-
-        JTextField dataTextField = new JTextField();
-        dataTextField.setText(dataField);
-        dataTextField.setEditable(false);  // The user cannot modify the data unless "Modify" is clicked
-        this.dataFields.add(dataTextField);
-
-        // Create the JButton to copy the data field
-        JButton copyDataButton = new JButton();
-        copyDataButton.setText("❏");
-        copyDataButton.setPreferredSize(new Dimension(50, 50));
-        copyDataButton.addActionListener(getCopyDataActionListener(dataField));
-        this.copyButtons.add(copyDataButton);
-
-        // Add the JTextField and the JButton to the panel
-        dataFieldPanel.add(dataTextField, BorderLayout.CENTER);
-        dataFieldPanel.add(copyDataButton, BorderLayout.EAST);
-        return dataFieldPanel;
     }
 
     /**
@@ -167,14 +139,9 @@ public class UserData extends JPanel {
      * Set the data to be edited
      */
     private void enableEditing(){
-        // Set the JTextFields to be editable to allow changes
-        for (JTextField dataField : this.dataFields){
-            dataField.setEditable(true);
-        }
-
-        // Set the copy buttons to not be enabled while the data is being changed
-        for (JButton copyButton : this.copyButtons){
-            copyButton.setEnabled(false);
+        // Set the DataFields to be editable to allow changes
+        for (UserDataField dataField : this.userDataFields){
+            dataField.enableEditing();
         }
 
         // Disable and hide the buttons that cannot be used while in Modify state
@@ -194,14 +161,9 @@ public class UserData extends JPanel {
      * Set the data to not be edited
      */
     private void disableEditing(){
-        // Set the JTextFields to not be editable to disable changes
-        for (JTextField dataField : this.dataFields){
-            dataField.setEditable(false);
-        }
-
-        // Set the copy buttons to be enabled again
-        for (JButton copyButton : this.copyButtons){
-            copyButton.setEnabled(false);
+        // Set the DataFields to not be editable to disable changes
+        for (UserDataField dataField : this.userDataFields){
+            dataField.disableEditing();
         }
 
         // Disable and hide the buttons that cannot be used while in Read-only state
@@ -223,45 +185,18 @@ public class UserData extends JPanel {
 
         // Iterate over the data fields to get their data and set them not to be editable
         // Set the new data to the copy button and enable it again
-        for (int i = 0; i < this.dataFields.size(); i++){
-            JTextField dataField = this.dataFields.get(i);
-            updatedData[i] = dataField.getText();
-            dataField.setEditable(false);
-
-            // Remove the old action listener, which is always at index 0, and create a new one with the updated data
-            JButton copyButton = this.copyButtons.get(i);
-            copyButton.removeActionListener(copyButton.getActionListeners()[0]);
-            copyButton.addActionListener(getCopyDataActionListener(dataField.getText()));
-            copyButton.setEnabled(true);
+        for (int i = 0; i < this.userDataFields.length; i++){
+            updatedData[i] = this.userDataFields[i].getData();
         }
 
         // Ensure that the service field is not empty
         if (updatedData[3].isEmpty()){
-            enableEditing();  // Enable to make changes again as they were disabled in the previous for loop
             return;
         }
 
-        // Send the data to the backend
-        changeData(new Data(this.ID, updatedData[0], updatedData[1], updatedData[2], updatedData[3], updatedData[4]));
+        // Disable the editing and send the data to the backend
         disableEditing();
-    }
-
-    /**
-     * Create an Action Listener to copy a dataField used by the buttons
-     * @param dataField The text which is copied to the clipboard
-     * @return ActionListener with actionPerformed method
-     */
-    private ActionListener getCopyDataActionListener(String dataField){
-        return new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Get the clipboard from the system
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                StringSelection dataToCopy = new StringSelection(dataField);  // Create a Transferable
-                clipboard.setContents(dataToCopy, dataToCopy);  // Copy the transferable
-                JOptionPane.showMessageDialog(getPanel(), "Copied the data to the clipboard.");
-            }
-        };
+        changeData(new Data(this.ID, updatedData[0], updatedData[1], updatedData[2], updatedData[3], updatedData[4]));
     }
 
     /**
@@ -282,13 +217,14 @@ public class UserData extends JPanel {
         this.itc.request(event);
     }
 
+    @Deprecated
     /**
      * Generate a password and set the password text field to it
      */
     private void generatePassword(){
         Event event = this.itc.requestAndReceive(new Event("generate-string"));
         String password = (String) event.getData().getFirst();
-        this.dataFields.get(2).setText(password);  // Set the password to the third text field: the password one
+//        this.userDataFields[2].setText(password);  // Set the password to the third text field: the password one
     }
 
     /**
