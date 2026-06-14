6b14bba94156b15f67ac0139ec958e35df619a6a
Forzo <96546068+Forzooo@users.noreply.github.com>
2026-06-14 23:27:22 +0200

    Correzioni test
    - Corretto l'errore dove prima di decifrare i dati non era stata impostata la master password
    - Aggiornato il test getDatabasePath alle ultime modifiche

--- src/test/java/moda/passwordmanager/backend/HelperTests.java
+++ src/test/java/moda/passwordmanager/backend/HelperTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,16 +27,17 @@ class HelperTests {
     private final static String DATABASE_ROOT_NODE = "database/";
 
     // Decryption test attributes
+    private final static String MASTER_PASSWORD = "dev";
     private final static String STRING_PLAINTEXT = "Test";
-    private final static String STRING_CIPHERTEXT = "aCRSwS7aFZXmixeSRHPxgLwHLXejH22ZkJAxfxDtqxtJWZBxhdQyyFGuM3+UNI3Z";
+    private final static String STRING_CIPHERTEXT = "yrflaOOdfwumdECCw4OaxY+R+myhKKr1XU843ieinqMx1Y7BGItZBL3+ybitpPbH";
     private final static Data DATA_PLAINTEXT = new Data(STRING_PLAINTEXT, STRING_PLAINTEXT, STRING_PLAINTEXT,
             STRING_PLAINTEXT, STRING_PLAINTEXT);
     private final static Data DATA_CIPHERTEXT = new Data(
-            "SvZNvHkFLzlNyYnAfiubsivzoD8kDCxs+WN1L6XObeQfRbSL469P+sWWZvLiB3+v",
-            "hqm3IG7983V5gbfxsyixvo4ZO6Ziajr9Hlds0zjgYstxcZqv6rhT2vfWHMqUWcPY",
-            "4X7xLUCgUs+5bDORr6mTUCAeDqR0NaCM53f+wuppHDMY6kNnE1uGIvBGQepRbtFZ",
-            "tmWEn5QoLbzoLOW4tGNCTSYHhgbQ9JGFfZnlIhRCVnPNKt+N0IpNTcair2KYVliI",
-            "m5pMeoBQ++fmEO/5FedHYXG5HkYdAYXczoSGOrkBZ25h2uzk+/NCWNeU/Wfu5YHm"
+            "cJ2pjIGv3/xrxKbcC+kf3Xdwj4eC6MFozR3ffiT5asBNCQkEBZn/JuixAYJYJmQP",
+            "YoIOEHEf5Bb+PSuaKa0Ctk6c/2SJqs90IWllpemEJp0KuGyDrQbKXE3T1tLW2m/d",
+            "sDTNHKDmEsrCCz/lFTUzptcwb3ZyWMDSqif/OvaNDjOTomCVrNHM+w92tNVQ97xS",
+            "jeMOllUlPG0P8YWzZ78Np5dH8KkXyvn4H+IkDiPq+aRgCMRuBAuPx3myq9/SkOTq",
+            "B12TW0JIRTJH5o12kVkWpo7v1rttKILjetBplc1RmnBr6I4cwoy9FLbU7js0t/Ac"
     );
 
     /**
@@ -48,6 +50,7 @@ class HelperTests {
     @BeforeEach
     void init(){
         Cryptography cryptography = new Cryptography();
+        cryptography.setMasterPassword(MASTER_PASSWORD.getBytes());
         this.settings = new Settings(TEST_DIRECTORY+"\\"+Helper.getSettingsFile());
         this.helper = new Helper(cryptography, settings);
     }
@@ -91,7 +94,7 @@ class HelperTests {
     @Test
     void getDatabasePath(){
         String databasePath = this.helper.getDatabasePath();
-        assertEquals(this.settings.readStringProperty(DATABASE_ROOT_NODE+"path"), databasePath);
+        assertEquals(this.settings.readStringProperty(DATABASE_ROOT_NODE+"selected"), databasePath);
     }
 
     /**
